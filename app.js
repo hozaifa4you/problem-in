8f078ddcb04e4183391d1301fcda53fa250ab894
yousuf4you <yousuf360.ya@gmail.com>
2020-07-25 16:04:49 +0600

    You have to help me

---
+++ app.js
@@ -0,0 +1,34 @@
+//Colors
+var colors = [
+    "rgb(0,255,255)",
+    "rgb(138,43,226)",
+    "rgb(0,139,139)",
+    "rgb(255,20,147)",
+    "rgb(0,191,255)",
+    "rgb(70,130,180)"
+];
+
+//Squares
+var square = document.querySelectorAll(".square");
+var displayColor = document.querySelector(".show-colors");
+var notice = document.querySelector(".notice");
+//random colors
+var colorPicker = colors[2];
+
+
+displayColor.textContent = colorPicker;
+//square loop
+
+for (let index = 0; index < square.length; index++) {
+    square[index].style.background = colors[index];
+    square[index].addEventListener("click", function() {
+        //compair Color
+        var compairWith = String(this.style.backgroundColor);
+
+        if (colorPicker === compairWith) {
+            console.log("You are right.");
+        } else {
+            console.log("You are wrong.");
+        }
+    });
+}
